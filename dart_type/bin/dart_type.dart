2421e6f1e584763676da107535adceb853621f03
silangli <1686066313@qq.com>
2021-10-18 07:57:40 +0000

    dart

--- dart_type/bin/dart_type.dart
+++ dart_type/bin/dart_type.dart
@@ -1,6 +1,7 @@
-//number--int,double
-
+//数字--int,double
+/*
 void main() {
+  var a=1.4;
   int x = 1;
   double y = 1.1;
   num z = 0;
@@ -15,4 +16,14 @@ void main() {
   String piAsString = 3.14159.toStringAsFixed(2);
   assert(piAsString == '3.14');
   print('$one ,$onePointOne ,$oneAsString ,$piAsString ');
+}
+*/
+//字符串--String
+void main() {
+  var s = 'string interprolation';
+  /*
+    在字符串中,以 ${表达式} 的形式使用表达式,
+    如果表达式是一个标识符，可以省略掉 {}
+  */
+  print('Dart has $s,which is very handy.');
 }
