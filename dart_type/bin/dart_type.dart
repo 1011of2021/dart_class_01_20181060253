a325ed66bc3fb8fdacd0152cde6ad4edaf6fcc53
silangli <1686066313@qq.com>
2021-10-18 07:27:00 +0000

    number

--- dart_type/bin/dart_type.dart
+++ dart_type/bin/dart_type.dart
@@ -1,3 +1,18 @@
-void main(List<String> arguments) {
-  print('Hello world!');
+//number--int,double
+
+void main() {
+  int x = 1;
+  double y = 1.1;
+  num z = 0;
+  print('$x ,$y ,$z ');
+  //字符串和数字之间转换
+  var one = int.parse('1');
+  assert(one == 1);
+  var onePointOne = double.parse('1.1');
+  assert(onePointOne == 1.1);
+  String oneAsString = 1.toString();
+  assert(oneAsString == '1');
+  String piAsString = 3.14159.toStringAsFixed(2);
+  assert(piAsString == '3.14');
+  print('$one ,$onePointOne ,$oneAsString ,$piAsString ');
 }
