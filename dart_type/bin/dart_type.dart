d3d82c4042b063439e9a98e87937f858fb502e27
silangli <1686066313@qq.com>
2021-10-18 08:15:03 +0000

    dart

--- dart_type/bin/dart_type.dart
+++ dart_type/bin/dart_type.dart
@@ -19,11 +19,35 @@ void main() {
 }
 */
 //字符串--String
+/*
 void main() {
   var s = 'string interprolation';
   /*
     在字符串中,以 ${表达式} 的形式使用表达式,
     如果表达式是一个标识符，可以省略掉 {}
   */
-  print('Dart has $s,which is very handy.');
+  assert('Dart has $s, which is very handy.' ==
+      'Dart has string interpolation, '
+          'which is very handy.');
+  //使用 + 运算符或并列放置多个字符串来连接字符串
+  var s1 = 'String'
+      'concatenation'
+      "works even over line breaks.";
+  assert(s1 ==
+      'String concatenation works even over'
+          'line breaks');
+  var s2 = 'The+operator' + 'works,as well.';
+  assert(s2 == 'The + operator works, as well.');
+  //使用三个单引号或者三个双引号能创建多行字符串
+  var s3 = '''
+      You can create
+      multi-line strings like this one.''';
+  var s4 = """This is also a
+      multi-line string.""";
+  //字符串前加上 r 作为前缀创建 “raw” 字符串,不会被做任何处理
+  var s5 = r'''在 raw 字符串中，
+      转义字符串 \n 会直接输出 “\n” 
+      而不是转义为换行。''';    
 }
+*/
+//布尔
