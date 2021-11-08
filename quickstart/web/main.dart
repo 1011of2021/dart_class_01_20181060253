2f49bcff4dd2211390fe407fe7131c6e1cc5c0e4
silangli <1686066313@qq.com>
2021-11-08 06:31:31 +0000

    web

--- quickstart/web/main.dart
+++ quickstart/web/main.dart
@@ -1,5 +1,10 @@
 import 'dart:html';
-
+Iterable<String> thingsTodo() sync* {  
+  for (int i = 0; i < 5; i++) {
+    print('hello ${i + 1}');
+  }
+ }
+ LIElement newLI(String itemText) => LIElement()..text = itemText;
 void main() {
-  querySelector('#output')?.text = 'Your Dart app is running.';
+  querySelector('#output')?.children.addAll(thingsTodo().map(newLI));
 }
